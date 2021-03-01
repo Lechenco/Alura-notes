556763fc37e80704571d426a6889a78b7398c93c
Gabriel Lechenco <glechenco@gmail.com>
2021-02-28 21:47:46 -0300

    First Servlet

---
+++ Spring/Activities/01_gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,28 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// super.service(arg0, arg1);
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi Mundo, Funcionaaa!");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+}
